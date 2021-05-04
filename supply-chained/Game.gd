8a6c056b65612b0e62a0d51461fd88d4836f4242
Zuberbühler <daniel.zuberbuehler@outlook.com>
2021-05-04 17:44:35 +0200

    created autoloaded RouteBuilder

--- supply-chained/Game.gd
+++ supply-chained/Game.gd
@@ -3,22 +3,9 @@ extends Node2D
 const Route = preload("res://router/Route.tscn");
 
 func _on_train_pressed():
-#	enable_stations(true)
-#	var station = null
-#	var steps = []
-#
-#	while len(steps) == 0 or station != steps[0]:
-#		if station != null:
-#			steps.append(station)
-#			enable_stations(true, station.get_connected_stations())
-#
-#		station = yield($Map, "station_selected") as Station
-#		enable_stations(false)
-#
-#	$Train1.visible = true
-#	var route = Route.instance().init(steps, $Train1)
-#	add_child(route)
-#	route.start()
+	var route = yield(get_node("/root/RouteBuilder").build_route($Train1, $Map), "completed")
+	add_child(route)
+	route.start()
 
 func enable_stations(enabled:bool, stations=get_tree().get_nodes_in_group('Station')):
 	for s in stations:
--- supply-chained/route-builder/RouteBuilder.gd
+++ supply-chained/route-builder/RouteBuilder.gd
@@ -2,24 +2,26 @@ extends Node
 
 const Route = preload("res://router/Route.tscn");
 
-func build_route(carrier):
+
+var steps = [];
+var pickups = [];
+
+func build_route(carrier, map):
 	enable_stations(true)
 	var station = null
-	var steps = []
+	steps = []
+	pickups = []
 
 	while len(steps) == 0 or station != steps[0]:
 		if station != null:
 			steps.append(station)
 			enable_stations(true, station.get_connected_stations())
 
-		station = yield($Map, "station_selected") as Station
+		station = yield(map, "station_selected") as Station
 		enable_stations(false)
 	
 	carrier.visible = true
-	var route = Route.instance().init(steps, carrier)
-	route.start()
-	
-	return route
+	return Route.instance().init(steps, carrier)
 
 func enable_stations(enabled:bool, stations=get_tree().get_nodes_in_group('Station')):
 	for s in stations:
