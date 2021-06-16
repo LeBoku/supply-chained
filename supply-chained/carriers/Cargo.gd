c52cf65da700bcd9db8baf6fc244f1f3560bea5f
Zuberbühler <daniel.zuberbuehler@outlook.com>
2021-06-16 18:16:21 +0200

    refactored cargo display handling

---
+++ supply-chained/carriers/Cargo.gd
@@ -0,0 +1,33 @@
+extends HBoxContainer
+
+signal carrier_selected(Carrier)
+
+const Util = preload("res://util/Util.gd")
+
+var carrier: Carrier
+onready var materials = $"/root/MaterialHelper"
+
+func initialize(carrier: Carrier):
+	self.carrier = carrier
+	$Name.text = carrier.name+" >"
+
+	for cargo in carrier.cargo:
+		var el = $CargoSpace.duplicate()
+		el.get_node("Payload").texture = null
+		add_child(el)
+		move_child(el, 0)
+
+	remove_child($CargoSpace)
+	
+	carrier.connect("cargo_changed", self, "display")
+	display()
+
+func display():
+	for index in range(len(carrier.cargo)):
+		var el = .get_child(index)
+		var texture = null
+
+		if carrier.cargo[index] != null:
+			texture = materials.get_icon(carrier.cargo[index])
+			
+		el.get_node("Payload").texture = texture
--- supply-chained/carriers/Hud.gd
+++ supply-chained/carriers/Hud.gd
@@ -9,26 +9,7 @@ onready var carrier: Carrier
 onready var materials = $"/root/MaterialHelper"
 
 func _ready():
-	carrier.connect("cargo_changed", self, "display")
+	$Cargo.initialize(carrier)
 
-	$Cargo/Name.text = carrier.name+" >"
-	Util.propagate_signal($Cargo/Name, "pressed", self, "carrier_selected", [carrier])
-
-	for cargo in carrier.cargo:
-		var el = $Cargo/CargoSpace.duplicate()
-		el.get_node("Payload").texture = null
-		$Cargo.add_child(el)
-		$Cargo.move_child(el, 0)
-
-	$Cargo.remove_child($Cargo/CargoSpace)
-	display()
-
-func display():
-	for index in range(len(carrier.cargo)):
-		var el = $Cargo.get_child(index)
-		var texture = null
-
-		if carrier.cargo[index] != null:
-			texture = materials.get_icon(carrier.cargo[index])
-			
-		el.get_node("Payload").texture = texture
+func _on_Name_pressed():
+	emit_signal("carrier_selected", carrier)
