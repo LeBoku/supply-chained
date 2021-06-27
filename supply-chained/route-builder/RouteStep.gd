5740e33dda45e50fdccba7f49e79df8e68e4df35
Zuberbühler <daniel.zuberbuehler@outlook.com>
2021-06-27 20:47:38 +0200

    sorted routestep exchanges

--- supply-chained/route-builder/RouteStep.gd
+++ supply-chained/route-builder/RouteStep.gd
@@ -12,3 +12,7 @@ func initialize(station: Station):
 
 func add_exchange(exchange: Array):
 	exchanges.append(exchange)
+	exchanges.sort_custom(self, "sort_exchanges")
+
+func sort_exchanges(a:Array, b:Array):
+	return not a[2] if a[2] != b[2] else a[1] < b[1]
