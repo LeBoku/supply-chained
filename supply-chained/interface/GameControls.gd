7dcc3e64430d6d17ad4af15b91eb95ebfe16effe
Zuberbühler <daniel.zuberbuehler@outlook.com>
2021-07-30 18:22:25 +0200

    added gamespeed ui

---
+++ supply-chained/interface/GameControls.gd
@@ -0,0 +1,24 @@
+extends HBoxContainer
+
+func set_game_speed(speed: float, button: Button):
+	$"/root/Globals".game_speed = speed
+	
+	$Stop.disabled = false
+	$Normal.disabled = false
+	$Faster.disabled = false
+	$Fastest.disabled = false
+	
+	button.disabled = true
+
+
+func _on_Stop_pressed():
+	set_game_speed(0, $Stop)
+
+func _on_Normal_pressed():
+	set_game_speed(1, $Normal)
+
+func _on_Faster_pressed():
+	set_game_speed(2, $Faster)
+
+func _on_Fastest_pressed():
+	set_game_speed(4, $Fastest)
